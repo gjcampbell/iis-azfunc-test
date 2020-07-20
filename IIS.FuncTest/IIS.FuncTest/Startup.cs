e04fb2e4dd67205126f87f1d3afb5a33bca963c6
Gabriel Campbell <gabriel.campbell@hrblock.com>
2020-07-19 19:27:07 -0500

    the whole thing

---
+++ IIS.FuncTest/IIS.FuncTest/Startup.cs
@@ -0,0 +1,27 @@
+﻿
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+using NLog;
+
+[assembly: FunctionsStartup(typeof(IIS.FuncTest.Startup))]
+namespace IIS.FuncTest
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var logFactory = LogManager.LoadConfiguration(GetLogConfigFilePath());
+            builder.Services.AddLogging(b => b.AddNLog());
+        }
+
+        private string GetLogConfigFilePath()
+        {
+            var assmPath = typeof(Startup).Assembly.Location;
+            var binDir = Path.GetDirectoryName(assmPath);
+            var result = Path.Combine(binDir, "..\\nlog.config");
+            return result;
+        }
+    }
+}
---
+++ IIS.FuncTest/IIS.FuncTest/Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace IIS.FuncTest
+{
+    public class Tests
+    {
+        private readonly ILogger<Tests> logger;
+
+        public Tests(ILogger<Tests> logger)
+        {
+            this.logger = logger;
+        }
+
+        [FunctionName(nameof(TestHttp))]
+        public IActionResult TestHttp(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+        {
+            logger.LogInformation("HttpTrigger ran");
+            return new OkObjectResult("Perfect! 🙃");
+        }
+
+        [FunctionName(nameof(TestQueue))]
+        public void TestQueue([QueueTrigger("iis-q-test")]string item)
+        {
+            logger.LogInformation($"QueueTrigger ran value: {item}");
+        }
+
+        [FunctionName(nameof(TestStorage))]
+        public void TestStorage([BlobTrigger("iis-blob-test/{name}")]Stream blob, string name)
+        {
+            logger.LogInformation($"BlobTrigger ran file name: {name}, size: {blob.Length}");
+        }
+
+        [FunctionName(nameof(TestTimer))]
+        public void TestTimer([TimerTrigger("*/20 * * * * *")]TimerInfo timer)
+        {
+            logger.LogInformation($"TimerTrigger ran");
+        }
+    }
+}
